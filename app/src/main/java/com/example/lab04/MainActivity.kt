171c7e22ba3ab8ec657171d904ef54e4ee5a4047
AngelV-Dev <angelv.27.25@gmail.com>
2026-04-12 20:50:19 -0500

    feat: Add progress bar components from Lab 03

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab04.ui.theme.Lab04Theme
 
 class MainActivity : ComponentActivity() {
@@ -44,4 +50,21 @@ fun GreetingPreview() {
     Lab04Theme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun MyProgressBar() {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(color = Color.Blue)
+        LinearProgressIndicator(modifier = Modifier.padding(top = 8.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProgressBar() {
+    MyProgressBar()
 }
